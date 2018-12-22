ad2367c435120b5d9cd5604c530383151355f5c2
fadibarcelona <41168516+fadibarcelona@users.noreply.github.com>
2018-12-23 04:26:17 +0500

    Update NewTest.java

--- src/main/java/example/NewTest.java
+++ src/main/java/example/NewTest.java
@@ -3,7 +3,8 @@ package example;
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;		
 import org.openqa.selenium.firefox.FirefoxDriver;		
-import org.testng.Assert;		
+import org.testng.Assert;	
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;	
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;		
@@ -13,6 +14,13 @@ public class NewTest {
 	    	  @Test
 	    	  public void openMyBlog() {
 	    	 driver.get("https://www.softwaretestingmaterial.com/");
+			  WebElement title = driver.findElement(By.xpath("//title"));
+
+       System.out.println("********************************************************");
+
+       System.out.println("*" + title.getAttribute("text") + "*");
+
+       System.out.println("********************************************************");
 	    	  }
 	    	  
 	    	  @BeforeTest

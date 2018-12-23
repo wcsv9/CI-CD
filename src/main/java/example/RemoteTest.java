1b687a49b529d7b009a60bda6fa81160f7c734d6
fadibarcelona <41168516+fadibarcelona@users.noreply.github.com>
2018-12-23 06:33:36 +0500

    Create RemoteTest.java

---
+++ src/main/java/example/RemoteTest.java
@@ -0,0 +1,26 @@
+package example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+
+
+
+public class RemoteTest {
+
+  public static void main(String[] args) throws Exception {
+    // Change this to match the location of your server
+    URL server = new URL("http://192.168.16:134:4444/wd/hub");
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setBrowserName("chrome");
+
+    System.out.println("Connecting to " + server);
+
+    WebDriver driver = new RemoteWebDriver(server, capabilities);
+
+    driver.get("http://www.google.com");
+
+    driver.quit();
+  }
+}

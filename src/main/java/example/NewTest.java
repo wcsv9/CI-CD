fb1005d6afaf3822494fc052f1ee46ffb760c786
fadibarcelona <41168516+fadibarcelona@users.noreply.github.com>
2018-12-23 06:33:13 +0500

    Update NewTest.java

--- src/main/java/example/NewTest.java
+++ src/main/java/example/NewTest.java
@@ -32,22 +32,3 @@ public class NewTest {
 	    	  }	
 }
 
-class RemoteTest {
-
-  public static void main(String[] args) throws Exception {
-    // Change this to match the location of your server
-    URL server = new URL("http://192.168.16.134:4444/wd/hub");
-
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setBrowserName("firefox");
-
-    System.out.println("Connecting to " + server);
-
-    WebDriver driver = new RemoteWebDriver(server, capabilities);
-
-    driver.get("http://www.google.com");
-
-    driver.quit();
-  }	
-}
-

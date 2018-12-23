1ae5e63410ce1d4a3f885a17d498490d9d1e28b8
fadibarcelona <41168516+fadibarcelona@users.noreply.github.com>
2018-12-23 05:44:38 +0500

    Update NewTest.java

--- src/main/java/example/NewTest.java
+++ src/main/java/example/NewTest.java
@@ -1,38 +1,22 @@
-package example;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
 
-import org.openqa.selenium.By;		
-import org.openqa.selenium.WebDriver;		
-import org.openqa.selenium.firefox.FirefoxDriver;		
-import org.testng.Assert;	
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;	
-import org.testng.annotations.BeforeTest;	
-import org.testng.annotations.AfterTest;		
-public class NewTest {		
-	    private WebDriver driver;		
+class RemoteTest {
 
-	    	  @Test
-	    	  public void openMyBlog() {
-	    	 driver.get("https://www.softwaretestingmaterial.com/");
-			  WebElement title = driver.findElement(By.xpath("//title"));
+  public static void main(String[] args) throws Exception {
+    // Change this to match the location of your server
+    URL server = new URL("http://192.168.16.134:4444/wd/hub");
 
-       System.out.println("********************************************************");
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setBrowserName("firefox");
 
-       System.out.println("*" + title.getAttribute("text") + "*");
+    System.out.println("Connecting to " + server);
 
-       System.out.println("********************************************************");
-	    	  }
-	    	  
-	    	  @BeforeTest
-	    	  public void beforeTest() {
-	    			System.setProperty("browser.gecko.driver", System.getProperty("user.dir")+"/geckodriver");
-//	    	   System.setProperty("webdriver.gecko.driver", "C:\\Users\\taha.patel\\eclipse-workspace2\\SampleProject\\geckodriver.exe");
-	    	   driver = new FirefoxDriver();
-	    	   
-	    	  }
-	    	 
-	    	  @AfterTest
-	    	  public void afterTest() {
-	    	   driver.quit();
-	    	  }	
-}	
+    WebDriver driver = new RemoteWebDriver(server, capabilities);
+
+    driver.get("http://www.google.com");
+
+    driver.quit();
+  }
